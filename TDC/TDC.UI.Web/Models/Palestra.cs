57ec5241482893257a578733840c10254ed5a70b
Juniior Belleza <belcprint@gmail.com>
2014-08-13 22:35:08 -0300

    Adicionado Models

---
+++ TDC/TDC.UI.Web/Models/Palestra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDC.UI.Web.Models
+{
+    public class Palestra
+    {
+        public string Titulo { get; set; }
+        public string Codigo { get; set; }
+        public string Nivel { get; set; }
+        public string Descricao { get; set; }
+        public Trilha Trilha { get; set; }
+        public Palestrante Palestrante { get; set; }
+        public Sala Sala { get; set; }
+        public DateTime Horario { get; set; }
+
+    }
+}
---
+++ TDC/TDC.UI.Web/Models/Palestrante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDC.UI.Web.Models
+{
+    public class Palestrante
+    {
+        public string nome { get; set; }
+        public string Twitter { get; set; }
+        public string Bio { get; set; }
+        public string fotoId { get; set; }
+    }
+}
